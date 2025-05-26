2872c5136c3d9a4943d75cebf989fa9aae9af61f
praveen037 <d.b.n.praveen0377@gmail.com>
2025-05-26 17:11:54 +0530

    Updated files

---
+++ frontend/src/services/EmployeeService.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+const EMPLOYEE_API = "http://localhost:9191/api/v1/employees";
+
+class EmployeeService {
+    addEmployee(employee) {
+        return axios.post(EMPLOYEE_API, employee);
+    }
+
+    getAllEmployees() { // ✅ Correct function name
+        return axios.get(EMPLOYEE_API);
+    }
+
+    getEmployeeById(employeeId) {
+        return axios.get(`${EMPLOYEE_API}/${employeeId}`);
+    }
+
+    updateEmployee(employeeId, employee) {
+        return axios.put(`${EMPLOYEE_API}/${employeeId}`, employee);
+    }
+
+    deleteEmployee(employeeId) {
+        return axios.delete(`${EMPLOYEE_API}/${employeeId}`);
+    }
+}
+
+export default new EmployeeService();
